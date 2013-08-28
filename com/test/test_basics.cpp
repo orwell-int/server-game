27cc385cddeec6bd3f6d89f4360bad030d35f0d0
fdiller <florent.diller@yahoo.fr>
2013-08-28 18:03:06 +0200

    manip avec Damien

--- com/test/test_basics.cpp
+++ com/test/test_basics.cpp
@@ -1,20 +1,20 @@
 // Test serialization and de-serialization of messages
 
 #include <iostream>
-#include <orwell_message.hpp>
+#include <message.hpp>
 #include <version1.pb.h>
 
 int main()
 {
-	orwell::com::orwell_message rmsg("LOGIN");
+	orwell::com::message rmsg("LOGIN");
 
-	rmsg.access_internal_message<orwell::com::login_message>().set_client_id("c_id");
-	rmsg.access_internal_message<orwell::com::login_message>().set_wished_robo_type("TANK");
+	rmsg.accessInternalMessage<orwell::com::loginMessage>().set_client_id("c_id");
+	rmsg.accessInternalMessage<orwell::com::loginMessage>().set_wished_robot_type("TANK");
 	rmsg.commit();
 
-	orwell::com::login_message const & mymsg(rmsg.get_internal_message<orwell::com::login_message>());
+	orwell::com::loginMessage const & mymsg(rmsg.getInternalMessage<orwell::com::loginMessage>());
 	if (mymsg.client_id() != "c_id") return 1;
-	if (mymsg.wished_robo_type() != "TANK") return 1;
+	if (mymsg.wished_robot_type() != "TANK") return 1;
 
 	return 0;
 }
