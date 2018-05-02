1d7a94178ad63b1e9a29261fb58b9455192c199a
dchilot <dchilot@hotmail.com>
2018-05-02 03:52:55 +0000

    Tweaks for OpenCV

--- test/test_opencv.cpp
+++ test/test_opencv.cpp
@@ -7,6 +7,8 @@
 
 #include "orwell/support/GlobalLogger.hpp"
 
+#include <gtest/gtest.h>
+
 int main()
 {
 	orwell::support::GlobalLogger::Create("test_opencv", "test_opencv.log", true);
@@ -15,6 +17,7 @@ int main()
 	cv::Mat aImage = cv::imread("circles.jpg", 0);
 	if (aImage.empty())
 	{
+		ORWELL_LOG_ERROR("Could not read image");
 		return -1;
 	}
 
@@ -29,15 +32,19 @@ int main()
 			200, 30, 0, 0   // param1, param2, minRadius, maxRadius
 			);
 	ORWELL_LOG_INFO("Circles detected: " << aCircles.size());
-	assert(1 == aCircles.size());
+	EXPECT_EQ(1, aCircles.size())
+		<< "Only one circlue should be found";
 	for (auto && aCircle : aCircles)
 	{
 		ORWELL_LOG_DEBUG(
 				"circle of radius " << aCircle[2] <<
 				" at (" << aCircle[0] << ", " << aCircle[1] << ")");
-		assert(abs(197.5 - aCircle[0]) < 1e-5); // X
-		assert(abs(199.5 - aCircle[1]) < 1e-5); // Y
-		assert(abs(94.3319 - aCircle[2]) < 1e-5); // radius
+		EXPECT_TRUE(abs(197.5 - aCircle[0]) < 1e-5)
+			<< "X coordinate";
+		EXPECT_TRUE(abs(199.5 - aCircle[1]) < 1e-5)
+			<< "Y coordinate";
+		EXPECT_TRUE(abs(94.3319 - aCircle[2]) < 1e-4)
+			<< "radius " << aCircle[2];
 		circle(aGrayScaleImage, cv::Point(aCircle[0], aCircle[1]), aCircle[2], cv::Scalar(0, 0, 255), 3);
 		circle(aGrayScaleImage, cv::Point(aCircle[0], aCircle[1]), 2, cv::Scalar(0, 255, 0), 3);
 	}
